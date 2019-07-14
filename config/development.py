bb14e55a2156e3ebb158a98ee39a6bb01e61cb1c
Nemanja Savkic <nemanja.savkic92@gmail.com>
2019-07-14 18:26:29 +0200

    - Changes in configuration - Code refactoring

--- config/development.py
+++ config/development.py
@@ -5,12 +5,12 @@ class Development(Config):
     ENV_TYPE =  "Development"
 
     DB_NAME = "praksa_2019"
-    DB_USER = "username"
-    DB_PASSWORD = "123"
+    DB_USER = "Nemanja"
+    DB_PASSWORD = "123456"
     DB_HOST = "127.0.0.1"
     DB_PORT = 5432
 
-    username = "Nikola"
+    username = "Nemanja"
     password = "123456"
 
     SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
